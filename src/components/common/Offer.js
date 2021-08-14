432f3ddd41b75a472292fff6d9e4f5cf8194e2a8
PHUONGNAM011000 <namhungvuong00@gmail.com>
2021-08-14 21:29:43 +0700

    thirst commit

--- src/components/common/Offer.js
+++ src/components/common/Offer.js
@@ -1,22 +1,31 @@
 import Exclusive from '../../assets/images/exclusive.png';
+import { Link } from 'react-router-dom';
 
 const Offer = () => {
-    return ( 
-        <div id="offer">
-            <div className="small-container">
-                <div className="row">
-                    <div className="col-2">
-                        <img src={Exclusive} alt="Exclusive" className="offer-img"/>
-                    </div>
-                    <div className="col-2">
-                        <p>Exclusive Available on RedStore</p>
-                        <h1>Smart Band 4</h1>
-                        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, fuga enim. Repellendus accusantium amet facere! Illum quos quis autem pariatur dignissimos voluptates vero dolores animi?</small>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="offer">
+      <div className="small-container">
+        <div className="row">
+          <div className="col-2">
+            <img src={Exclusive} alt="Exclusive" className="offer-img" />
+          </div>
+          <div className="col-2">
+            <p>Exclusive Available on RedStore</p>
+            <h1>Smart Band 4</h1>
+            <small>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste,
+              fuga enim. Repellendus accusantium amet facere! Illum quos quis
+              autem pariatur dignissimos voluptates vero dolores animi?
+            </small>
+            <br />
+            <Link className="btn" to="#">
+              Buy Now &#8594;
+            </Link>
+          </div>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Offer;
